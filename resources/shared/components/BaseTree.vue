0082434873298c79c96adf7461b3051f62b5e983
Алексей Цапаев <aleksei.tsapaev@yasno.live>
2025-07-31 00:14:51 +0300

    234

--- resources/shared/components/BaseTree.vue
+++ resources/shared/components/BaseTree.vue
@@ -1,21 +1,16 @@
 <template>
-    <div>
-        <h3>{{ title }}</h3>
-        <TreeTable :value="treeNodes" size="small">
-            <Column
-                v-for="col of columns"
-                :key="col.field"
-                :field="col.field"
-                :header="col.header"
-            />
-            <template #footer>
-                <div class="flex ">
-                    <div class="flex-1"/>
-                    <Button type="button" icon="pi pi-plus" text @click="onCreate" />
-                </div>
-            </template>
-        </TreeTable>
-    </div>
+  <div>
+    <h3>{{ title }}</h3>
+    <TreeTable :value="treeNodes" size="small">
+      <Column v-for="col of columns" :key="col.field" :field="col.field" :header="col.header" />
+      <template #footer>
+        <div class="flex">
+          <div class="flex-1" />
+          <Button type="button" icon="pi pi-plus" text @click="onCreate" />
+        </div>
+      </template>
+    </TreeTable>
+  </div>
 </template>
 
 <script setup lang="ts" generic="T extends IBaseTreeEntity<T> & IBaseEntity">
@@ -25,62 +20,28 @@ import Column from "primevue/column";
 import type { TreeNode } from "primevue/treenode";
 import type { IBaseEntity, IBaseTreeEntity, IBaseColumn } from "../types";
 import type { Component } from "vue";
-import { useRightDrawer } from "../composables/useRightDrawer";
-import type { RightDrawerData, RightDrawerProps } from "../types/rightDrawer";
 
-const {
-    columns = [],
-    title = "",
-    createComponent,
-    createTitle = "Создание элемента",
-    createProps,
-} = defineProps<{
-    columns?: IBaseColumn[];
-    title?: string;
-    createComponent?: Component;
-    createTitle?: string;
-    createProps?: RightDrawerProps;
-}>();
-
-const emit = defineEmits<{
-    create: [data: RightDrawerData];
+const { columns = [], title = "" } = defineProps<{
+  columns?: IBaseColumn[];
+  title?: string;
 }>();
 
 const items = defineModel<T[]>("items", {
-    default: () => [],
+  default: () => [],
 });
 
-const { push } = useRightDrawer();
-
 const treeNodes = computed<TreeNode[]>(() => {
-    return items.value.map((item) => transformToTreeNode(item));
+  return items.value.map((item) => transformToTreeNode(item));
 });
 
 function transformToTreeNode(item: T): TreeNode {
-    return {
-        key: item.id,
-        data: item,
-        ...item,
-        children: item.children
-            ? item.children.map((child) => transformToTreeNode(child))
-            : undefined,
-    };
+  return {
+    key: item.id,
+    data: item,
+    ...item,
+    children: item.children ? item.children.map((child) => transformToTreeNode(child)) : undefined,
+  };
 }
 
-const onCreate = (): void => {
-    if (createComponent) {
-        push(createComponent, {
-            title: createTitle,
-            props: createProps,
-            onSave: (data: RightDrawerData) => {
-                emit("create", data);
-            },
-            onClose: () => {
-                console.log("Создание отменено");
-            },
-        });
-    } else {
-        console.log("onCreate: компонент создания не задан");
-    }
-};
+const onCreate = (): void => {};
 </script>
