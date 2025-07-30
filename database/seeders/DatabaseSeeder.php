d0107a20073cacd92754555c551a4ed6a0b1156e
Алексей Цапаев <aleksei.tsapaev@yasno.live>
2025-07-30 23:50:22 +0300

    аа

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
             PermissionSeeder::class,
             UserSeeder::class,
             RestaurantRolesSeeder::class,
+            RestaurantSeeder::class,
         ]);
     }
 }
