5f5a1021a9c9687ada6512a84a055d58615e187c
Алексей Цапаев <aleksei.tsapaev@yasno.live>
2025-08-02 21:26:26 +0300

    rest files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             RestaurantRolesSeeder::class,
             RestaurantSeeder::class,
+            FileSeeder::class,
             NewsSeeder::class,
         ]);
     }
