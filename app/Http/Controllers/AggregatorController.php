09adad627286e8c5ab4005476da725a332d08d62
Алексей Цапаев <aleksei.tsapaev@yasno.live>
2025-09-17 01:25:14 +0300

    324

--- app/Http/Controllers/AggregatorController.php
+++ app/Http/Controllers/AggregatorController.php
@@ -2,11 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\Models\Restaurant;
+
 class AggregatorController extends Controller
 {
-    public function site()
+    public function site(Request $request)
     {
-        return view('site');
+        $yandexMetricaCode = null;
+
+        $any = $request->route('any');
+        if (is_string($any) && $any !== '') {
+            $slug = explode('/', $any)[0];
+            $yandexMetricaCode = Restaurant::query()
+                ->where('slug', $slug)
+                ->value('yandex_metrica_code');
+        }
+
+        return view('site', [
+            'yandex_metrica_code' => $yandexMetricaCode,
+        ]);
     }
 
     public function account()
--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -8,6 +8,26 @@
     <title>Eat.Drink.Repeat</title>
     <script src="https://telegram.org/js/telegram-web-app.js?59"></script>
     @vite(['resources/site/css/app.css', 'resources/site/ts/app/main.ts'])
+    @if (!empty($yandex_metrica_code))
+    <script>
+        (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0];
+            k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+        ym({!! json_encode($yandex_metrica_code) !!}, 'init', {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true
+        });
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/{{ $yandex_metrica_code }}" style="position:absolute; left:-9999px;" alt="" /></div>
+    </noscript>
+    @endif
 </head>
 
 <body class="antialiased">
